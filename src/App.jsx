c44dd2bfa3fa52876ed947ed74fabb0d719005d6
codesofpreeti <preetiipriyaa@gmail.com>
2023-11-19 14:04:12 +0530

    props and tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 // import { useState } from 'react'
 
 import './App.css'
-import Counter from './components/Counter'
+// import Counter from './components/Counter'
+import Card from './components/Card'
 
 function App() {
 
   return (
     <>
-     <Counter/>
+    {/*<Counter/>*/}
+    <h1 className='bg-green-400 text-black p-4 rounded mb-4'>tailwind css</h1>
+    <Card userName="Preeti" />
+    <Card btnText="check me out"/>
     </>
   )
 }
